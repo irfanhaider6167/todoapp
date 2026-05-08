867c5a1840b8a1ed1660b547bc49024a2c4e2ead
irfanhaider6167 <101730155+irfanhaider6167@users.noreply.github.com>
2026-05-08 21:07:29 +0500

    meta tags don

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import { BrowserRouter as Router } from "react-router-dom";
-import Layout from "./layout/Layout";
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter } from "react-router-dom";
 import { AppProvider } from "./context/todoContex";
+
+// Lazy Load Layout
+const Layout = lazy(() => import("./layout/Layout"));
+
+// Loading Component
+function Loading() {
+  return (
+    <div className="flex items-center justify-center h-screen text-xl font-semibold">
+      Loading...
+    </div>
+  );
+}
 
 function App() {
   return (
-    <AppProvider>
-      <Router>
-        <Layout />
-      </Router>
-    </AppProvider>
+    <BrowserRouter>
+      <AppProvider>
+        <Suspense fallback={<Loading />}>
+          <Layout />
+        </Suspense>
+      </AppProvider>
+    </BrowserRouter>
   );
 }
 
